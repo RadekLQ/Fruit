6e93b18123a579bf17847ee7b4c5f3f9d09055cc
Radek <radoslaw.lizak@gmail.com>
2017-09-15 12:40:58 +0200

    cleaned-up v 1.0.1 functional

--- src/main/java/be/vdab/fruit/FruitList.java
+++ src/main/java/be/vdab/fruit/FruitList.java
@@ -1,10 +1,8 @@
 package be.vdab.fruit;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FruitList {
     private List<Fruit> fruitLijst = new ArrayList<>();
@@ -15,23 +13,6 @@ public class FruitList {
     public void add(Fruit fruit) {
         fruitLijst.add(fruit);
     }
-
-
-//    public void add() {
-//        fruitLijst.stream().collect(Collectors.toList());
-//    }
-
-//    public void add() {
-//        fruitLijst.addAll(Arrays.asList());
-//    }
-
-//    public void add() {
-//        fruitLijst.addAll(fruitLijst.stream().collect(Collectors.toList()));
-//    }
-
-//    public void add() {
-//        fruitLijst.addAll(new ArrayList<>(fruitLijst));
-//    }
 
     public void sorteerVolgensPrijs() {
         Collections.sort(fruitLijst);
